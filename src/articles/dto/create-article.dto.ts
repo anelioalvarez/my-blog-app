c025826f10e3e3df92e18caac7db95c74dd7305a
darrell <anelio272014@gmail.com>
2023-05-11 17:39:27 -0300

    Add validation rules to CreateArticleDto

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,15 +1,34 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateArticleDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @MinLength(5)
+  @IsString()
   title: string;
 
   @ApiPropertyOptional()
+  @MaxLength(300)
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   description?: string;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   body: string;
 
   @ApiPropertyOptional({ default: false })
+  @IsBoolean()
+  @IsOptional()
   published?: boolean = false;
 }
